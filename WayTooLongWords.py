46c36a6f8775735a821e73a547b9f80cae398916
Meiirzhan00 <54979320+Meiirzhan00@users.noreply.github.com>
2021-01-16 18:19:12 +0600

    Create WayTooLongWords.py

---
+++ WayTooLongWords.py
@@ -0,0 +1,17 @@
+class WayTooLongWords:
+  def get(self) :
+    x=[]
+    n=int(input('How many words do you enter ?\n'))
+    while n>0:
+      s=input()
+      x.append(s)
+      n-=1
+    for i in x:
+      if len(i)<10:
+        print(i)
+      else:
+        print(i[0]+str(len(i[1:len(i)-1]))+i[len(i)-1])
+
+p=WayTooLongWords()
+p.get()
+
