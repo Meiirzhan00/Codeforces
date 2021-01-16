2d977ec0bfac510b73a523176c6df6e4d2ae2a53
Meiirzhan00 <54979320+Meiirzhan00@users.noreply.github.com>
2021-01-16 18:53:58 +0600

    Update WayTooLongWords.py

--- WayTooLongWords.py
+++ WayTooLongWords.py
@@ -2,16 +2,22 @@ class WayTooLongWords:
   def get(self) :
     x=[]
     n=int(input('How many words do you enter ?\n'))
-    while n>0:
-      s=input()
-      x.append(s)
-      n-=1
-    for i in x:
-      if len(i)<10:
-        print(i)
-      else:
-        a,*b,c=i
-        print(a+str(len(b))+c)
+    if 1<=n<=100:
+      while n>0:
+        s=input()
+        x.append(s)
+        n-=1
+      for i in x:
+        if 1<=len(i)<=100:
+          if len(i)<10:
+            print(i)
+          else:
+            a,*b,c=i
+            print(a+str(len(b))+c)
+        else:
+          print(exit(1))
+    else:
+      print(exit(1))
 
 p=WayTooLongWords()
 p.get()
